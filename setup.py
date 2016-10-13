5dd58cddeb76eba86e25fb6c06e0004a4102b975
alex <alex>
2016-10-13 13:17:32 -0400

    Update setup.py for PyPI packaging

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(name="cast_convert",
+      version="0.1",
+      description="Convert and inspect video for Chromecast playback",
+      url="https://github.com/thismachinechills/cast_convert",
+      author="thismachinechills (Alex)",
+      license="AGPL 3.0",
+      packages=['cast_convert'],
+      zip_safe=True,
+      install_requires=["click"],
+      keywords=["chromecast", "ffmpeg", "transcode"],
+      )
