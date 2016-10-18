c4397ef05a107b8f86055d5480b9ed3a80ea9a8a
alex <alex>
2016-10-18 03:50:41 -0400

    Add version and version command

--- cast_convert/__init__.py
+++ cast_convert/__init__.py
@@ -1,4 +1,16 @@
+__version__ = '0.1.5.18'
+
+
+from .cmd import cmd as command
+from .watch import *
 from . import *
 from .convert import *
 from .media_info import *
-from .watch import *
+
+
+@click.command(help="Print version")
+def version():
+    print(__version__)
+
+
+command.add_command(version)
