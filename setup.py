b702ea80aba171fe6c34f80a6493a759d661d912
Alex DeLorenzo <alex@alexdelorenzo.dev>
2022-10-29 19:50:43 -0400

    Upgrade setup script

--- setup.py
+++ setup.py
@@ -10,16 +10,18 @@ OLD_PYTHON_REQUIREMENTS = ['mypy-lang']
 REQUIREMENTS = Path('requirements.txt').read_text().split('\n')
 
 if version_info < MIN_PYTHON_VERSION:
-    REQUIREMENTS.extend(OLD_PYTHON_REQUIREMENTS)
+  REQUIREMENTS.extend(OLD_PYTHON_REQUIREMENTS)
 
-setup(name="cast_convert",
-      version=__version__,
-      description="Convert and inspect video for Chromecast playback",
-      url="https://github.com/alexdelorenzo/cast_convert",
-      author="alexdelorenzo.dev (Alex DeLorenzo)",
-      license="AGPL 3.0",
-      packages=['cast_convert'],
-      zip_safe=True,
-      install_requires=REQUIREMENTS,
-      keywords=REQUIREMENTS,
-      entry_points={"console_scripts": ["cast_convert = cast_convert:command"]})
+setup(
+  name="cast_convert",
+  version=__version__,
+  description="Convert and inspect video for Chromecast playback",
+  url="https://github.com/alexdelorenzo/cast_convert",
+  author="alexdelorenzo.dev (Alex DeLorenzo)",
+  license="AGPL 3.0",
+  packages=['cast_convert'],
+  zip_safe=True,
+  install_requires=REQUIREMENTS,
+  keywords=REQUIREMENTS,
+  entry_points={"console_scripts": ["cast_convert = cast_convert:command"]}
+)
