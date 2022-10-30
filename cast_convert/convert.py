a075dc0683074c7f78aeb9f4a3c4cd17128a6525
Alex DeLorenzo <alex@alexdelorenzo.dev>
2022-10-30 01:53:33 -0400

    Add `convert` module

--- cast_convert/convert.py
+++ cast_convert/convert.py
@@ -1,9 +1,59 @@
 from __future__ import annotations
 
-from .base import VideoProfile, VideoCodec, AudioProfile, AudioCodec, Container, Formats
+from typing import Final
+
+from .base import VideoProfile, VideoCodec, AudioProfile, AudioCodec, Container, Formats, \
+  Extension
+from .exceptions import UnknownFormat
 from .video import Video, VideoMetadata
 from .device import Device
 
+import ffmpeg
+
+
+DEFAULT_EXT: Final[Extension] = Container.matroska.to_extension()
+
 
 def transcode_video(video: Video, formats: Formats) -> Video:
-  pass
+  container, video_profile, audio_profile = video.formats
+  video_codec, resolution, fps, level = video_profile
+  [audio_codec] = audio_profile
+
+  _, video_profile, audio_profile = formats
+
+  if formats.container:
+    try:
+      new_ext = formats.container.to_extension()
+
+    except UnknownFormat as e:
+      new_ext = DEFAULT_EXT
+
+  else:
+    new_ext = video.path.suffix
+
+  new_path = (
+    video.path
+    .with_stem(f'{video.path.stem}_transcoded')
+    .with_suffix(new_ext)
+  )
+
+  args: dict[str, str] = {}
+
+  if formats.audio_profile.codec:
+    args['acodec'] = 'copy'
+
+  else:
+    args['acodec'] = ''
+
+  if formats.video_profile.codec:
+    args['vcodec'] = 'copy'
+
+  stream = (
+    ffmpeg
+    .input(video.path)
+    .output(new_path, **args)
+  )
+  stream.run()
+
+  raise NotImplementedError("Not done yet")
+  return Video.from_path(new_path)
