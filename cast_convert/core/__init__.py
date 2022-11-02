8218af0cd054fabbdf02a843214af5729fd1c13f
Alex DeLorenzo <alex@alexdelorenzo.dev>
2022-11-02 01:04:46 -0400

    Export all submodules to top-level module

--- cast_convert/core/__init__.py
+++ cast_convert/core/__init__.py
@@ -1,1 +1,5 @@
-from . import base, exceptions, parse, rules, convert, media, model
+from . import base, exceptions, parse, rules
+from . import model, media, convert
+from .model import *
+from .media import *
+from .convert import *
