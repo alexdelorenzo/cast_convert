07ebe28a2b9dfa904839354e9febff145e4a5e04
Alex DeLorenzo <alex@alexdelorenzo.dev>
2022-11-07 22:47:57 -0500

    Add `convert`, `inspect` and `devices` commands to CLI

--- cast_convert/cli/helpers.py
+++ cast_convert/cli/helpers.py
@@ -5,12 +5,32 @@ from typing import Final
 from rich import print
 
 from ..core.base import first
-from ..core.convert.run import get_ffmpeg_cmd, get_stream
+from ..core.convert.run import get_ffmpeg_cmd, get_stream, transcode_video
 from ..core.model.device import Device
 from ..core.model.video import Video
+from ..core.parse import DEVICE_INFO
 
 
 DEFAULT_MODEL: Final[str] = 'Chromecast 1st Gen'
+
+
+def get_device(
+  name: str,
+  devices: Path = DEVICE_INFO,
+) -> Device | None:
+  devices: tuple[Device] = tuple(Device.from_yaml())  # type: ignore
+
+  if not (device := first(dev for dev in devices if dev.name == name)):
+    print(f'[b red]Device name "{name}" not found[/b red], please use one of these:')
+    show_devices(devices)
+    return
+
+  return device
+
+
+def show_devices(devices: tuple[Device]):
+  for device in devices:
+    print(f'\t - [b]{device.name}')
 
 
 def _get_command(
@@ -18,15 +38,8 @@ def _get_command(
   path: Path,
 ):
   video = Video.from_path(path)
-  devices: tuple[Device] = tuple(Device.from_yaml())  # type: ignore
-  device: Device
 
-  if not (device := first(dev for dev in devices if dev.name == name)):
-    print(f'[b red]Device name "{name}" not found[/b red], please use one of these: ')
-
-    for device in devices:
-      print(f'\t - [b]{device.name}')
-
+  if not (device := get_device(name)):
     return
 
   if device.can_play(video):
@@ -38,3 +51,38 @@ def _get_command(
 
   cmd = get_ffmpeg_cmd(stream)
   print(f'[b]{cmd}')
+
+
+def _convert(
+  name: str,
+  path: Path,
+):
+  video = Video.from_path(path)
+
+  if not (device := get_device(name)):
+    return
+
+  if device.can_play(video):
+    print(f'No need to transcode {video.name} for {device.name}.')
+    return
+
+  formats = device.transcode_to(video)
+  transcode_video(video, formats)
+
+
+def _inspect(
+  name: str,
+  path: Path,
+):
+  video = Video.from_path(path)
+
+  if not (device := get_device(name)):
+    return
+
+  if device.can_play(video):
+    print(f'No need to transcode {video.name} for {device.name}.')
+    return
+
+  print(f'These attributes will be converted from  {video.path}:\n\t', end='')
+  formats = device.transcode_to(video)
+  print(formats)
--- cast_convert/cli/orig.py
+++ cast_convert/cli/orig.py
@@ -4,17 +4,76 @@ from pathlib import Path
 from typing import Final
 
 from typer import Typer, Option, Argument
+from rich import print
 
-from .helpers import DEFAULT_MODEL, _get_command
+from .helpers import DEFAULT_MODEL, _convert, _get_command, _inspect, show_devices
+from ..core.model.device import Device
+
+
+DEFAULT_NAME_OPT: Final[Option] = Option(
+  default=DEFAULT_MODEL,
+  help='Chromecast model name',
+)
+
+
+DEFAULT_PATHS_ARG: Final[Argument] = Argument(
+  default=None,
+  help='Path, or paths, to video(s)',
+  resolve_path=True,
+)
 
 
 app: Final[Typer] = Typer()
 
 
-@app.command(help='Get FFMPEG transcoding command.')
+@app.command()
 def get_command(
-  name: str = Option(DEFAULT_MODEL, help='Chromecast model name'),
-  paths: list[Path] = Argument(None, help='Path to video', resolve_path=True),
+  name: str = DEFAULT_NAME_OPT,
+  paths: list[Path] = DEFAULT_PATHS_ARG,
 ):
+  """
+  Get FFMPEG transcoding command.
+  """
+
   for path in paths:
     _get_command(name, path)
+
+
+@app.command()
+def convert(
+  name: str = DEFAULT_NAME_OPT,
+  paths: list[Path] = DEFAULT_PATHS_ARG,
+):
+  """
+  Convert video for Chromecast compatibility.
+  """
+
+  for path in paths:
+    _convert(name, path)
+
+
+@app.command()
+def inspect(
+  name: str = DEFAULT_NAME_OPT,
+  paths: list[Path] = DEFAULT_PATHS_ARG,
+):
+  """
+  Inspect a video to see what attributes should be decoded.
+  """
+
+  for path in paths:
+    _inspect(name, path)
+
+
+@app.command()
+def devices():
+  """
+  List all supported device names.
+  """
+  devices: tuple[Device] = tuple(Device.from_yaml())  # type: ignore
+  print('You can use these device names with the [b]--name[/b] flag: ')
+  show_devices(devices)
+
+
+
+
