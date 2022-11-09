0e8e6e12a2eea5c1109e34849256f4e84d77663b
Alex DeLorenzo <alex@alexdelorenzo.dev>
2022-11-09 03:47:38 -0500

    Remove old code and unused dependencies

--- cast_convert/core/convert/watch.py
+++ cast_convert/core/convert/watch.py
@@ -9,9 +9,6 @@ from typing import AsyncGenerator, AsyncIterable, Final
 
 from aiopath import AsyncPath
 from watchfiles import awatch, Change
-from hachiko.hachiko import AIOEventHandler
-from watchdog.events import PatternMatchingEventHandler
-from watchdog.utils.patterns import match_any_paths
 
 from ...cli.helpers import DEFAULT_MODEL, _convert
 from ..model.device import Device
@@ -108,82 +105,3 @@ async def convert_videos(
     async for path in gen_videos(*paths, seen=seen):
       coro = convert(device, path, sem)
       tg.create_task(coro)
-
-
-### watchdog ###
-
-@dataclass
-class AddedFileHandler(
-  AIOEventHandler
-):
-  patterns: list[str] | None = None
-  excluded: list[str] | None = None
-  case_sensitive: bool = True
-  seen: set[Path] = field(default_factory=set[Path])
-  debug: bool = False
-  queue: Queue = field(default_factory=Queue)
-
-  def matches(self, *paths: Path) -> bool:
-    return match_any_paths(
-      paths,
-      included_patterns=self.patterns,
-      excluded_patterns=self.excluded,
-      case_sensitive=self.case_sensitive,
-    )
-
-  def dispatch(self, event):
-    logging.debug(f'Handling {event}')
-    paths: list[str] = []
-
-    if hasattr(event, 'dest_path'):
-      paths.append(os.fsdecode(event.dest_path))
-
-    if event.src_path:
-      paths.append(os.fsdecode(event.src_path))
-
-    if self.matches(*paths):
-      return super().dispatch(event)
-
-  async def on_moved(self, event):
-    filename = event.dest_path
-
-    if self.debug:
-      logging.debug(event)
-      logging.debug((filename, 'seen', filename in self.seen))
-
-    if filename not in self.seen:
-      await self.queue.put(filename)
-      self.seen.add(filename)
-
-  async def on_created(self, event):
-    return await self.on_moved(event)
-
-
-async def consume_video_queue(
-  queue: Queue,
-  threads: int = DEFAULT_THREADS,
-  show_debug: bool = False
-):
-  while True:
-    name = await queue.get()
-
-    if show_debug:
-      logging.debug("Getting %s size..." % name)
-
-    size = await wait_for_stable_size(name)
-
-    if show_debug:
-      logging.debug(name, size)
-      logging.debug("Determine if", name, "is a video...")
-
-    is_vid = await is_video(name)
-
-    if show_debug:
-      logging.debug(name, is_vid)
-      logging.debug("Converting video...")
-
-    if is_vid:
-      pass
-
-    else:
-      print(name, 'not a video.')
