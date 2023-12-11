e1085e582ae27fe3b18d39cd6c11f0730371754b
alexdelorenzo <3579286+alexdelorenzo@users.noreply.github.com>
2023-12-10 23:12:44 -0500

    Bump year

--- cast_convert/__init__.py
+++ cast_convert/__init__.py
@@ -2,14 +2,14 @@ from __future__ import annotations
 from typing import Final
 
 
-__version__: Final[str] = '0.3.0'
+__version__: Final[str] = '0.13.0'
 
 NAME: Final[str] = 'cast_convert'
 PROJECT_HOME: Final[str] = 'https://github.com/alexdelorenzo/cast_contvert'
 AUTHOR_HOME: Final[str] = 'https://alexdelorenzo.dev'
 AUTHOR: Final[str] = 'Alex DeLorenzo'
 AUTHOR_INFO: Final[str] = f'{AUTHOR} ({AUTHOR_HOME})'
-COPYRIGHT_NOTICE: Final[str] = f'Copyright © 2022 [b]{AUTHOR_INFO}[/b]'
+COPYRIGHT_NOTICE: Final[str] = f'Copyright © 2023 [b]{AUTHOR_INFO}[/b]'
 LICENSE: Final[str] = 'CC BY-NC-ND 4.0'
 
 DESCRIPTION: Final[str] = \
